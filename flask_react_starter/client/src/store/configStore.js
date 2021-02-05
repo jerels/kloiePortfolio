c1278d8e9fbf916535f34301d6835cd1e6720471
Jerel <65048179+jerels@users.noreply.github.com>
2021-02-04 20:48:10 -0600

    finished intit store config

--- flask_react_starter/client/src/store/configStore.js
+++ flask_react_starter/client/src/store/configStore.js
@@ -2,3 +2,17 @@ import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 let storeEnhancer;
+
+if (process.env.NODE_ENV !== 'production') {
+    const composeEnhancers = composeEnhancers(applyMiddleware(thunk));
+} else {
+    storeEnhancer = applyMiddleware(thunk);
+}
+
+export default function configStore(initState) {
+    return createStore(
+        rootReducer,
+        initState,
+        storeEnhancer
+    )
+};
