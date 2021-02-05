439ee48f620968306d59a95f2a0b79c064365301
Jerel <65048179+jerels@users.noreply.github.com>
2021-02-04 21:42:42 -0600

    added session to the store

--- flask_react_starter/client/src/store/configStore.js
+++ flask_react_starter/client/src/store/configStore.js
@@ -1,7 +1,12 @@
 import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import session from './session';
 
 let storeEnhancer;
+
+const rootReducer = combineReducers({
+    session
+})
 
 if (process.env.NODE_ENV !== 'production') {
     const composeEnhancers = composeEnhancers(applyMiddleware(thunk));
