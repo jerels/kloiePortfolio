80424cc8199518c841fb4301c93d015b8176755b
Jerel <65048179+jerels@users.noreply.github.com>
2021-02-04 21:57:44 -0600

    added the store to the index

--- flask_react_starter/client/src/index.js
+++ flask_react_starter/client/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import configStore from './store/configStore';
+
+export const store = configStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
