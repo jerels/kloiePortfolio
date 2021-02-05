2102a2193d432d131ffda90163b7cd730a03fc5c
Jerel <65048179+jerels@users.noreply.github.com>
2021-02-04 22:10:50 -0600

    added the auth route to app

--- flask_react_starter/client/src/App.js
+++ flask_react_starter/client/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 
 import UserList from './components/UsersList';
+import AuthRoute from './components/AuthRoute';
 
 
 function App() {
@@ -18,20 +19,12 @@ function App() {
 
     return (
         <BrowserRouter>
-            <nav>
-                <ul>
-                    <li><NavLink to="/" activeclass="active">Home</NavLink></li>
-                    <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
-                </ul>
-            </nav>
             <Switch>
                 <Route path="/users">
                     <UserList />
                 </Route>
 
-                <Route path="/">
-                    <h1>My Home Page</h1>
-                </Route>
+                <AuthRoute path='/' component={ } />
             </Switch>
         </BrowserRouter>
     );
