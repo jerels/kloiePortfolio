1ebdf60e1676759dece7469b23ab9b4eb9e213df
Jerel <65048179+jerels@users.noreply.github.com>
2021-02-04 21:51:04 -0600

    added the csrf useEffect to the app file

--- flask_react_starter/client/src/App.js
+++ flask_react_starter/client/src/App.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 
 import UserList from './components/UsersList';
 
 
 function App() {
-  console.log("____Rendering app_____")
-  return (
-    <BrowserRouter>
-        <nav>
-            <ul>
-                <li><NavLink to="/" activeclass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
-            </ul>
-        </nav>
-        <Switch>
-            <Route path="/users">
-                <UserList />
-            </Route>
 
-            <Route path="/">
-                <h1>My Home Page</h1>
-            </Route>
-        </Switch>
-    </BrowserRouter>
-  );
+    useEffect(() => {
+        const getCSRF = async () => {
+            const res = await fetch('/api/session/csrf');
+            if (res.ok) {
+                return;
+            }
+        }
+        getCSRF();
+    }, []);
+
+    return (
+        <BrowserRouter>
+            <nav>
+                <ul>
+                    <li><NavLink to="/" activeclass="active">Home</NavLink></li>
+                    <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path="/users">
+                    <UserList />
+                </Route>
+
+                <Route path="/">
+                    <h1>My Home Page</h1>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
